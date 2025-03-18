8b2c633cbf5d82c4e40e8e34d1b8d1183cc0891a
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2025-03-18 18:03:31 +0100

    Fix some build issues.

--- src/app/api/organizers/api.types.ts
+++ src/app/api/organizers/api.types.ts
@@ -1,5 +1,5 @@
+import { Organizer } from '@/organizer/types/organizer.types';
 import { ApiResponseList } from '../api.types';
-import { Organizer } from '@/src/organizer/organizer.types';
 
 export interface OrganizerApiResponse extends ApiResponseList {
   data: Organizer[];
