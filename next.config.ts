cabe29bdace36fb22ac564aaddf4d480153eb492
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2025-03-19 15:46:32 +0100

    Lint.

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false, // SwaggerUI uses some non-strict React features
